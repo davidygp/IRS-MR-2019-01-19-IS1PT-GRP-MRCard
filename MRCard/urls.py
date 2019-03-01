d6cfb79d4b5945a1ec20211ca945d98340d055db
Joe <leleljh@gmail.com>
2019-03-01 21:19:17 +0800

    update js link

--- MRCard/urls.py
+++ MRCard/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', include('Recommender.urls')),
     path('Recommender/', include('Recommender.urls')),
     path('admin/', admin.site.urls),
 ]
