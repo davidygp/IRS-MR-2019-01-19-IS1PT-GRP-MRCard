2014adddbacf3b8c72c87844f9e197a12b8c4d98
Joe <leleljh@gmail.com>
2019-03-01 21:23:05 +0800

    Use home page as recommender

--- MRCard/urls.py
+++ MRCard/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('Recommender.urls')),
-    path('Recommender/', include('Recommender.urls')),
+    # path('Recommender/', include('Recommender.urls')),
     path('admin/', admin.site.urls),
 ]
